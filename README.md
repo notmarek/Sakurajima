523e33d0d25e2e5f31b5f0bdde747f3c420c8576
Marek Vesely <veselym@spstrutnov.cz>
2020-06-10 23:12:13 +0200

    Release

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Sakurajima
+Sakurajima is a Python 3 API wrapper for AniWatch.me and contains most endpoints.
 ## Usage
 1. Install the package `pip install sakurajima`
 2. Use it.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='sakurajima',  
-      version='0.1.3',
+      version='0.1.4',
       license='MIT',
       author="Not Marek",
       author_email="notmarek@animex.tech",
