60d0c8b2e238f6fae0c7b1748383d60db1318c96
Marek Vesely <veselym@spstrutnov.cz>
2020-06-10 23:04:56 +0200

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='sakurajima',  
-      version='0.1.2',
+      version='0.1.3',
       license='MIT',
       author="Not Marek",
       author_email="notmarek@animex.tech",
