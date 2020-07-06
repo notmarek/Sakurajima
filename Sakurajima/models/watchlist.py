bd1183397a9d34bf63ededc1dd7cc1199b565272
DhanrajHira <hiradhanraj0100@gmail.com>
2020-07-06 23:16:38 +0530

    Added a missing import in the watchlist.py file

--- Sakurajima/models/watchlist.py
+++ Sakurajima/models/watchlist.py
@@ -1,3 +1,5 @@
+from Sakurajima.models import base_models as bm
+
 class WatchListEntry(object):
     def __init__(self, data_dict, headers, cookies, api_url):
         self.__headers = headers
@@ -15,7 +17,7 @@ class WatchListEntry(object):
         self.cover = data_dict.get("cover", None)
         self.available_episodes = data_dict.get("available_episodes", None)
         self.episodes = [
-            Episode(data, self.__headers, self.__cookies, self.__API_URL, self.anime_id)
+            bm.Episode(data, self.__headers, self.__cookies, self.__API_URL, self.anime_id)
             for data in data_dict.get("episodes", [])
         ]
 
